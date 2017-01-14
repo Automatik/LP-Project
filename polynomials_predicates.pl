08f6b0d50788f6b76b42503c9d25fc04bfb77b17
pox96 <simmy.pox@gmail.com>
2017-01-14 10:39:09 +0100

    Sistemazione pprint_polynomial
    Aggiunto parse_polynomial e risolto un bug

--- polynomials_predicates.pl
+++ polynomials_predicates.pl
@@ -736,8 +736,9 @@ as_poly(E, poly(Monomials)) :-
 
 %CASO GENERALE, per controllo che sia un polinomio
 pprint_polynomial(Poly) :-
-	is_polynomial(Poly),
-	pprint_poly(Poly).
+	parse_polynomial(Poly,Polyp),
+	is_polynomial(Polyp),
+	pprint_poly(Polyp).
 pprint_poly(poly([m(C, _, [v(N, V) | Vs]) | Ps])) :-
 	C\=[],
 	C\=0,
@@ -746,7 +747,7 @@ pprint_poly(poly([m(C, _, [v(N, V) | Vs]) | Ps])) :-
 	N\=1,
 	write(+),
 	write(C*V^N),
-	pprint_polynomial(poly([m([], _, Vs) | Ps])).
+	pprint_poly(poly([m([], _, Vs) | Ps])).
 
 pprint_poly(poly([m(C, _, [v(N, V) | Vs]) | Ps])) :-
 	C\=[],
@@ -754,7 +755,7 @@ pprint_poly(poly([m(C, _, [v(N, V) | Vs]) | Ps])) :-
 	C<0,
 	N\=1,
 	write(C*V^N),
-	pprint_polynomial(poly([m([], _, Vs) | Ps])).
+	pprint_poly(poly([m([], _, Vs) | Ps])).
 
 pprint_poly(poly([m(C, _, [v(N, V) | Vs]) | Ps])) :-
 	C\=[],
@@ -763,7 +764,7 @@ pprint_poly(poly([m(C, _, [v(N, V) | Vs]) | Ps])) :-
 	N\=1,
 	write(+),
 	write(V^N),
-	pprint_polynomial(poly([m([], _, Vs) | Ps])).
+	pprint_poly(poly([m([], _, Vs) | Ps])).
 
 pprint_poly(poly([m(C, _, [v(1, V) | Vs]) | Ps])) :-
 	C\=[],
@@ -772,14 +773,14 @@ pprint_poly(poly([m(C, _, [v(1, V) | Vs]) | Ps])) :-
 	C\=1,
 	write(+),
 	write(C*V),
-	pprint_polynomial(poly([m([], _, Vs) | Ps])).
+	pprint_poly(poly([m([], _, Vs) | Ps])).
 
 pprint_poly(poly([m(C, _, [v(1, V) | Vs]) | Ps])) :-
 	C\=[],
 	C\=0,
 	C<0,
 	write(C*V),
-	pprint_polynomial(poly([m([], _, Vs) | Ps])).
+	pprint_poly(poly([m([], _, Vs) | Ps])).
 
 pprint_poly(poly([m(C, _, [v(1, V) | Vs]) | Ps])) :-
 	C\=[],
@@ -787,7 +788,7 @@ pprint_poly(poly([m(C, _, [v(1, V) | Vs]) | Ps])) :-
 	C=1,
 	write(+),
 	write(V),
-	pprint_polynomial(poly([m([], _, Vs) | Ps])).
+	pprint_poly(poly([m([], _, Vs) | Ps])).
 
 pprint_poly(poly([m(C, _, [_ | _]) | Ps])) :-
 	C=0,
@@ -799,7 +800,7 @@ pprint_poly(poly([m([], _, [v(N, V) | Vs]) | Ps])) :-
 	N\=1,
 	write(*),
 	write(V^N),
-	pprint_polynomial(poly([m([], _, Vs) | Ps])).
+	pprint_poly(poly([m([], _, Vs) | Ps])).
 
 pprint_poly(poly([m([], _, [v(N, V)]) | Ps])) :-
 	N\=1,
@@ -811,18 +812,17 @@ pprint_poly(poly([m([], _, [v(1, V) | Vs]) | Ps])) :-
 	Vs\=[],
 	write(*),
 	write(V),
-	pprint_polynomial(poly([m([], _, Vs) | Ps])).
+	pprint_poly(poly([m([], _, Vs) | Ps])).
 
 pprint_poly(poly([m([], _, [v(1, V)]) | Ps])) :-
 	write(*),
 	write(V),
-	pprint_polynomial(poly(Ps)).
+	pprint_poly(poly(Ps)).
 
 pprint_poly(poly([m([], _, []) | Ps])) :-
-		pprint_polynomial(poly(Ps)).
+		pprint_poly(poly(Ps)).
 
-pprint_poly(poly([])) :-
-	fail.
+pprint_poly(poly([])).
 
 parse_polynomial(poly([m(C, G, Vps) | Ms]), poly([m(C1, G, Vps) | Ms1])) :-
 	arithmetic_expression_value(C, C1),
